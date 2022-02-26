52c45315286a883a572c58cd50914466ea336984
benb116 <benb116@gmail.com>
2022-02-26 11:04:40 -0500

    Move from congress:chapter to year:number
    Can be used with short titles

--- LawNetwork/BuildGraph.py
+++ LawNetwork/BuildGraph.py
@@ -27,11 +27,27 @@ with open('map.txt') as f:
         refpln = getPLN(ref)
         if (refpln != pln):
           # Old laws can't reference new laws
-          con1 = pln.split('-')[0]
-          con2 = refpln.split('-')[0]
-          if len(con2) > 3:
-            con2 = 1000
-          if int(con2) <= int(con1):
-            G.add_edge(pln, refpln)
+          con1 = pln.split('-')[0].split(':')[0]
+          con2 = refpln.split('-')[0].split(':')[0]
+          a = len(con1)
+          b = len(con2)
+          if a > 4 or b > 4:
+            continue
+          
+          i = int(con1)
+          j = int(con2)
+
+          if a < 4 and b < 4:
+            if j <= i:
+              G.add_edge(pln, refpln)
+          elif a < 4 and b >= 4:
+            if i * 2 + 1787 > j:
+              G.add_edge(pln, refpln)
+          elif a >= 4 and b < 4:
+            if j * 2 + 1787 < i:
+              G.add_edge(pln, refpln)
+          else:
+            if j <= i:
+              G.add_edge(pln, refpln)
 
 print(list(G.edges()))
